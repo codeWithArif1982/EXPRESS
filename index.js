68ca67d28a6411a556e32dec3df9e80f4df34a7d
codeWithArif1982 <125276843+codeWithArif1982@users.noreply.github.com>
2023-09-24 12:57:56 -0400

    route include

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ app= express();
 
 app.get("/", function(req,res){
     res.send("Hello Express Js");
-})
+});
+app.get("/about", function(req,res){
+    res.send("About Page");
+});
+app.get("/Contact", function(req,res){
+    res.send("Contact Page");
+});
+app.get("/Term", function(req,res){
+    res.send("Term Page");
+});
 
 app.listen(8000, function(){
     console.log("Server run Success");
