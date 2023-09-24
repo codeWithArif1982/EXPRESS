1da8108a860ab60ed4280e2ba93bb4fe4419b2be
codeWithArif1982 <125276843+codeWithArif1982@users.noreply.github.com>
2023-09-24 13:39:17 -0400

    simple string response added

---
+++ simpleStringResponse.js
@@ -0,0 +1,19 @@
+
+var express = require('express');
+
+app= express();
+
+//Simple String Response
+//res.send() --> response body
+//res.end()--> end of the body
+app.get("/one", function(req,res){
+    res.send("This is simple string response one");
+});
+
+app.post("/two", function(req,res){
+    res.end("This is simple string response two");
+});
+
+app.listen(8000, function(){
+    console.log("Server run Success");
+})
