4e6112287714278c91641cef8004b357612fdf4d
codeWithArif1982 <125276843+codeWithArif1982@users.noreply.github.com>
2023-09-26 17:48:44 -0400

    post header request

--- Request/simplePostQueryRequest.js
+++ Request/simplePostQueryRequest.js
@@ -3,7 +3,9 @@ var express = require('express');
 app = express();
 
 app.post('/', function(req,res){
-    res.send("This is simple post request");
+    let firstName = req.query.firstName;
+    let lastName = req.query.lastName;
+    res.send(firstName + " " + lastName);
 });
 
 app.listen(8000, function(){
